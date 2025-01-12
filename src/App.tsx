9a9272f25d67626d8ecfd660af6792b4d3ec2f87
ruben <rubenmobile2@gmail.com>
2025-01-12 23:18:15 +0400

    fixed build in main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { routeTree } from './routeTree.gen'
-import {mockApi} from '@/api/mockApi.ts';
 
 const router = createRouter({
     routeTree,
     defaultPreload: 'intent',
 })
-
-await mockApi.getContacts()
 
 export default function App() {
     return (
